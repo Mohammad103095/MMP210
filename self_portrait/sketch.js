55d6d50ac87d56163ed5b8344371a431e36d2347
S-NCERE <S-NCERE@users.noreply.github.com>
2018-09-06 20:30:10 -0700

    Self Portrait Added

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,9 +1,26 @@
 function setup() {
 	createCanvas(640, 360);
+    background(220);
 }
 
 function draw() {
-	background(220);
-	ellipse(320, 80, 100);
-    rect(50,50,200,100);
+    // fill("#FEB616")
+	stroke('black');
+    fill('nofill');
+    rect (220, 90, 200, 240);
+    
+    //Left Eye
+    fill('brown')
+    ellipse(270, 150, 50);
+    
+    //Right Eye
+    ellipse(370, 150, 50);
+    
+    //Mouth
+    stroke("red");
+    fill('red')
+    arc(320, 250, 100, 100, 0, PI);
+    
+    
+
 }
