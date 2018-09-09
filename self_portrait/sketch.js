cc1d44efb260915e8a1d3dec53086405bc9b2f60
Mohammad Islam <Mohdtislam103095@gmail.com>
2018-09-09 15:46:10 -0400

    Finish Self portrait 1

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -6,20 +6,43 @@ function setup() {
 function draw() {
     // fill("#FEB616")
 	stroke('black');
-    fill('nofill');
+    fill('tan');
     rect (220, 90, 200, 240);
     
     //Left Eye
-    fill('brown')
+    stroke('#0DFF84');
+    fill('#0CE83A');
     ellipse(270, 150, 50);
     
+    //Left Pupil
+    stroke('#17FF00');
+    fill('#0DFF84');
+    ellipse(270, 150, 30);
+    
     //Right Eye
+    stroke('#0DFF84');
+    fill('#0CE83A');
     ellipse(370, 150, 50);
+    
+    //Right Pupil
+    stroke('#17FF00');
+    fill('#0DFF84');
+    ellipse(370, 150, 30);
+    
+    //Noise
+    stroke("orange");
+    fill('orange');
+    arc(320, 200, 50, 50, 0, PI);
     
     //Mouth
     stroke("red");
-    fill('red')
+    fill('red');
     arc(320, 250, 100, 100, 0, PI);
+    
+    //Teeth
+    stroke("yellow");
+    fill('white');
+    arc(320, 255, 75, 75, 0, PI);
     
     
 
